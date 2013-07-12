0d28c29e7c223c9ba6798d69dbb15d3e55bd966c
Greg Saoutine <gsaoutine@hotmail.com>
2013-07-12 03:29:21 +0000

    Added buffers

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 var infile = "index.html";
+var buf = require ('buf');
 
 app.get('/', function(request, response) {
  fs.readFileSync(infile);
